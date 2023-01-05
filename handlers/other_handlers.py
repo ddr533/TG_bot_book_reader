5252bdfdd67a58df37baacd40a6caee92d76f9e1
Дрогаль Андрей <ddr533@yandex.ru>
2023-01-05 17:06:45 +0700

    Add handler for others messages

---
+++ handlers/other_handlers.py
@@ -0,0 +1,9 @@
+from aiogram import Dispatcher
+from aiogram.types import Message
+
+
+def init_other_handlers(dp: Dispatcher):
+    @dp.message_handler(content_types='text')
+    async def send_echo(message: Message):
+        '''Этот хэндлер будет реагировать на любые текстовые сообщения пользователя.'''
+        await message.answer(f'Это эхо! {message.text}')
