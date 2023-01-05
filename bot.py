e6c7f4df5b164bd560ed60be7a5ab1b9c5ea0f3c
Дрогаль Андрей <ddr533@yandex.ru>
2023-01-05 21:07:28 +0700

    Add users_handlers and book.txt for test_reading

---
+++ bot.py
@@ -0,0 +1,51 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+from handlers.other_handlers import init_other_handlers
+from handlers.user_handlers import init_user_handlers
+from keyboards.main_menu import set_main_menu
+
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+async def main() -> None:
+    '''Функция конфигурирования и запуска бота'''
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    config: Config = load_config('.env')
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp: Dispatcher = Dispatcher(bot)
+
+    # Настраиваем главное меню бота
+    await set_main_menu(dp)
+
+    # Инициализируем хэндлеры
+    init_user_handlers(dp)
+    init_other_handlers(dp)
+
+    # Запускаем polling
+    try:
+        await dp.skip_updates()
+        await dp.start_polling()
+
+    finally:
+        await bot.close()
+
+
+if __name__ == '__main__':
+    try:
+        # Запускаем функцию main
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        # Выводим в консоль сообщение об ошибке,
+        # если получены исключения KeyboardInterrupt или SystemExit
+        logger.error('Bot stopped!')
--- services/file_handling.py
+++ services/file_handling.py
@@ -4,20 +4,20 @@ PAGE_SIZE = 1050
 book: dict[int, str] = {}
 
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-    symbols = ',.!:;?'
-    end = start + size
-    if text[start:end][-1] not in symbols:
-        return _get_part_text(text, start, size - 1)
-    if text[start:end + 1][-1] in symbols:
-        if text[start:end + 1] != text[start:end]:
-            return _get_part_text(text, start, size - 1)
-        return text[start:end], len(text[start:end])
-    return text[start:end], size
-
+    ch=',.!:;?'
+    ssize=size
+    if len(text)<=size+start:
+        ssize=len(text)-start
+    else:
+        for i in range (size+start-1,start,-1):
+            if text[i] in ch and text[i+1] not in ch:
+                break
+            ssize-=1
+    return text[start: start + ssize], ssize
 
 def prepare_book(path: str) -> None:
     '''Функция, формирующая словарь книги.'''
-    with open(path, 'r') as file:
+    with open(path, 'r', encoding='UTF-8') as file:
         text = file.read()
         start, page_number = 0, 1
     while start < len(text):
@@ -25,6 +25,5 @@ def prepare_book(path: str) -> None:
         start += page_size
         book[page_number] = page_text.strip()
         page_number += 1
-
 
 prepare_book(BOOK_PATH)
