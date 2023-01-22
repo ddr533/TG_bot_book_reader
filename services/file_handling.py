b9eca42bee7cbd7093a2f5a8586ef196c9cbaedc
Дрогаль Андрей <ddr533@yandex.ru>
2023-01-22 15:35:28 +0700

    Change path to book using os.path

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,4 +1,6 @@
-BOOK_PATH = r'C:\Users\AndreyPC\PycharmProjects\TG_bot_book_reader\book\book.txt'
+import os
+
+BOOK_PATH = os.path.join(os.path.curdir, r'book\\book.txt')
 PAGE_SIZE = 1050
 
 book: dict[int, str] = {}
@@ -18,17 +20,16 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
 
 
 def prepare_book(path: str) -> None:
-    '''Функция, формирующая словарь книги.'''
+    """Функция, формирующая словарь книги."""
     with open(path, 'r', encoding='UTF-8') as file:
         text = file.read()
         start, page_number = 0, 1
     while start < len(text):
         page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
         start += page_size
-        page_text = page_text.replace('\n','')
+        page_text = page_text.replace('\n', '')
         book[page_number] = page_text.strip()
         page_number += 1
 
 
 prepare_book(BOOK_PATH)
-
